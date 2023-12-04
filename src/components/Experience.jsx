a9d347fd9174b41e14ad8d79bbbce2e967492a9e
Abi <johndoe@example.com>
2023-12-04 14:14:29 +0530

    three fiber

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+// import { Avatar } from "./Avatar";
+import { Office } from "./Office";
+
+export const Experience = () => {
+  return (
+    <>
+    <ambientLight intensity={1}/>
+    <Office/>
+    {/* <Avatar/> */}
+    </>
+  );
+};
